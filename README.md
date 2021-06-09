93318fdb86e8f90a64f07f74040397f9430452a8
Singing Lin <linsinging1234@gmail.com>
2021-06-09 10:12:09 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+## test
+`npm i singing-npm-package-first-test`
+
+## use
+```
+import rng from 'singing-npm-package-first-test';
+
+rng(5, 10);
+```
+
+
+## update readme
---
+++ index.js
@@ -0,0 +1,8 @@
+const randomNumber = (min = 0, max = 100) => {
+  // return Math.round((Math.random() * (max - min)) / min);
+  return 'V1.0.1same';
+};
+
+// console.log(randomNumber(5, 95));
+
+export default randomNumber;
