583384d79e8a094e1a09f7c7217af0ea347cacaf
Singing Lin <linsinging1234@gmail.com>
2021-06-09 16:29:50 +0800

    test: v1.0.3 change test

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const randomNumber = (min = 0, max = 100) => {
   // return Math.round((Math.random() * (max - min)) / min);
-  return 'V1.0.2';
+  return 'V1.0.3';
 };
 
 // console.log(randomNumber(5, 95));
